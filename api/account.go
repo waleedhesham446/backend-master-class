c5b2bbd18bd20a0cf11349491b6eb33f28ba19ea
0xwaleedh <waleedhesham2001@gmail.com>
2023-08-20 15:00:31 +0200

    #16 Handle DB errors in Golang correctly

--- api/account.go
+++ api/account.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/lib/pq"
 	db "github.com/waleedhesham446/backend-master-class/database/sqlc"
 
 	"github.com/gin-gonic/gin"
@@ -29,6 +30,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation", "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
